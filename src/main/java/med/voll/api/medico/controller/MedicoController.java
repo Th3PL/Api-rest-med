05f904bec065fdb8a3e46e63cbcf574a2e06a87b
Pedro Lucas <plucas2013@gmail.com>
2025-04-19 12:41:46 -0500

    Pradonizando respostas medico

--- src/main/java/med/voll/api/medico/controller/MedicoController.java
+++ src/main/java/med/voll/api/medico/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,27 +19,31 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+        return null;
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size=5, sort={"nome"}) Pageable paginacao){
+    public ResponseEntity<Page<DadosListagemMedico>>  listar(@PageableDefault(size=5, sort={"nome"}) Pageable paginacao){
 
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void remover(@PathVariable Long id){
+    public ResponseEntity remover(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.excluir();
+        return ResponseEntity.noContent().build();
     }
 }
