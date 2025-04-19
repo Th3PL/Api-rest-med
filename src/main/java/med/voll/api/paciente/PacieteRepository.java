5b064c3f2d87894c3e94e8d55ee01eea0ab234ce
Pedro Lucas <plucas2013@gmail.com>
2025-04-19 13:16:49 -0500

    Correção exclusão lógica paciente

--- src/main/java/med/voll/api/paciente/PacieteRepository.java
+++ src/main/java/med/voll/api/paciente/PacieteRepository.java
@@ -1,6 +1,10 @@
 package med.voll.api.paciente;
 
+import aj.org.objectweb.asm.commons.Remapper;
+import med.voll.api.medico.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacieteRepository extends JpaRepository<Paciente, Long> {
-}
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);}
