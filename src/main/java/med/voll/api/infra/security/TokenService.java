2363a188dd8bb697620e2db3e3696eb9aed2befd
Pedro Lucas <plucas2013@gmail.com>
2025-04-29 07:15:56 -0500

    geração token

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritimo = Algorithm.HMAC256("12354684");
+            return  JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(dataEspiracao())
+                    .sign(algoritimo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataEspiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
