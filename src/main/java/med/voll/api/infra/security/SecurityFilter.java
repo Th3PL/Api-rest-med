2d3002fcf1bb18cdeaf4b8977a278cc02ba5cefa
Pedro Lucas <plucas2013@gmail.com>
2025-05-05 10:02:33 -0500

    validando token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,19 +13,25 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var tokenJWT = recuperaToken(request);
-        System.out.println(tokenJWT);
+        var tokenJWT = recuperarToken(request);
+
+        var subject = tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
+
         filterChain.doFilter(request, response);
     }
 
-    private String recuperaToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
         }
 
-        return authHeader.replace("Bearer ", "");
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
